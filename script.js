00e678f67eaa402bcb10533e7d7ac00f7acf8a00
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-08-06 21:41:19 +0600

    Add file reader and print file input content

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const file = document.getElementById("spotifyle");
 
-file.addEventListener("change", (e) => {
-    const selected = e.target.files[0];
+file.onchange = (e) => {
+    const selected = e.target.files[0];    // select first one from the array :cry:
 
-    document.getElementById("filename").textContent = selected.name;
-})
+    // document.getElementById("filename").textContent = selected.name;
+
+    const reader = new FileReader();        // inside bcz creates a new one for each selected file
+
+    reader.onload = (e) => {             // after reading run this
+        console.log(reader.result);     // prints what the reader read
+    }
+
+    reader.readAsText(selected);    // starts with file to be read
+}
