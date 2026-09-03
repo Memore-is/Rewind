7a8d304d6509a11bb822c0301ba1c8141cd2ee30
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-09-04 00:24:00 +0600

    Print file info to page and each track name into console

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ file.onchange = (e) => {
 
     const reader = new FileReader();        // inside bcz creates a new one for each selected file
 
-    reader.onload = (e) => {             // after reading run this
-        console.log(reader.result);     // prints what the reader read
+    reader.onload = (e) => {
+        const data = JSON.parse(reader.result)  // create an array of strings from read text
+        
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].master_metadata_track_name);
+        }
+
+        document.getElementById("songn").textContent = data[0].master_metadata_track_name;
+        //console.log(data);     // prints the array
     }
 
     reader.readAsText(selected);    // starts with file to be read
